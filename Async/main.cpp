39ec7a830ec2605cd7f134345a5e8119c19ab1b0
Jason Reisman <jasonreisman@gmail.com>
2015-09-13 16:54:31 -0400

    - Turned main into a more unit test like file

--- Async/main.cpp
+++ Async/main.cpp
@@ -1,26 +1,15 @@
 #include "Async/Task.h"
 
+#include <cassert>
+#include <cmath>
 #include <iostream>
 
 namespace Test
 {
     const uint32_t TestQueue1 = 444;
     const uint32_t TestQueue2 = 999;
-    const uint32_t NumThreads = 1;
+    const uint32_t NumThreads = 4;
 }
-
-enum FooState
-{
-    FooState_A,
-    FooState_B,
-    FooState_C
-};
-
-enum FooTransition
-{
-    FooTransition_1,
-    FooTransition_2,
-};
 
 void setupQueues()
 {
@@ -31,45 +20,94 @@ void setupQueues()
     Async::registerQueue(queue2);
 }
 
-
-int main(int argc, const char* argv[])
+void testBasicTasks()
 {
-    auto noop = [](FooState, FooState, FooTransition) {};
-    Util::StateMachineT<FooState, FooTransition> sm(FooState_A);
-    sm.addTransition(FooState_A, FooState_B, FooTransition_1, noop);
-    FooState next = sm.executeTransition(FooTransition_1);
-    FooState next2 = sm.executeTransition(FooTransition_1);
-    
-    setupQueues();
-    
     Async::Task<int> f_int(Test::TestQueue1, []() {
-        std::cout << "Hello 444" << std::endl;
         return 444;
     });
     
-    Async::Task<int> f_int2 = f_int.then([](int x){
-        std::cout << "Hello " << (x+1) << std::endl;
-        return x+1;
+    int y_int = f_int.get();
+    assert(y_int == 444);
+    
+    Async::Task<double> f_double(Test::TestQueue1, []() {
+        return M_PI;
     });
+    
+    double y_double = f_double.get();
+    assert(fabs(y_double - M_PI) < 1e-8);
+    
+    Async::Task<std::string> f_str(Test::TestQueue1, []() {
+        return "Hello World";
+    });
+    
+    std::string y_str = f_str.get();
+    assert(y_str == "Hello World");
+}
 
-    Async::Task<void> f_intThenVoid = f_int.then([](int x){
-        std::cout << "Goodbye " << 2*x << std::endl;
+void testCreateTask()
+{
+    Async::Task<int> f_int = Async::CreateTask(Test::TestQueue1, []() {
+        return 444;
     });
     
     int y_int = f_int.get();
-    int y_int2 = f_int2.get();
-    f_intThenVoid.wait();
+    assert(y_int == 444);
     
-    Async::Task<int> f_int3 = f_int.then([](int x){
-        std::cout << "Hello " << (x+2) << std::endl;
-        return x+2;
+    Async::Task<double> f_double = Async::CreateTask(Test::TestQueue1, []() {
+        return M_PI;
     });
-    int y_int3 = f_int3.get();
     
-    Async::Task<void> f_void(Test::TestQueue1, [](){
-        std::cout << "Hello" << std::endl;
+    double y_double = f_double.get();
+    assert(fabs(y_double - M_PI) < 1e-8);
+    
+    Async::Task<std::string> f_str = Async::CreateTask(Test::TestQueue1, []() -> std::string {
+        return "Hello World";
     });
-    f_void.get();
+    
+    std::string y_str = f_str.get();
+    assert(y_str == "Hello World");
+}
+
+void testContinuationTasks()
+{
+    Async::Task<int> f_int = Async::CreateTask(Test::TestQueue1, []() {
+        return 444;
+    }).then([](int x) {
+        return 2*x + 1;
+    });
+    
+    int y_int = f_int.get();
+    assert(y_int == 889);
+    
+    Async::Task<double> f_double = Async::CreateTask(Test::TestQueue1, []() {
+        return M_PI;
+    }).then([](double x) {
+        return 2*x + 1;
+    });
+    
+    double y_double = f_double.get();
+    assert(fabs(y_double - (2*M_PI + 1)) < 1e-8);
+    
+    Async::Task<std::string> f_str = Async::CreateTask(Test::TestQueue1, []() -> std::string {
+        return "Hello World";
+    }).then([](const std::string& s) {
+        std::string rev = s;
+        std::reverse(rev.begin(), rev.end());
+        return rev;
+    });
+    
+    std::string y_str = f_str.get();
+    assert(y_str == "dlroW olleH");
+}
+
+
+int main(int argc, const char* argv[])
+{
+    setupQueues();
+    
+    testBasicTasks();
+    testCreateTask();
+    testContinuationTasks();
     
     return 0;
 }
